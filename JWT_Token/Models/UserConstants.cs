e2c016faf2b9f323ccea5bc349bdca7b4bc0647f
Vishal Maruti Patil <vishalmil@cybage.com>
2025-12-23 12:36:47 +0530

    Second commit jwt authentication

--- JWT_Token/Models/UserConstants.cs
+++ JWT_Token/Models/UserConstants.cs
@@ -4,7 +4,7 @@
     {
         public static List<UserModel> users = new List<UserModel>()
         {
-            new UserModel(){UserName="CybageAdmin", Password="CybageAdmin@123", Role="Admin"}
+            new UserModel(){UserName="TestAdmin", Password="TestAdmin@123", Role="Admin"}
         };
     }
 }
